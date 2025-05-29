4ceeaf44f73239ad69818167477603c737a8736c
tanvir-dev2000 <tanvirrahman0700@gmail.com>
2025-05-29 16:36:20 +0600

    trying to figure out login issues again

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -82,7 +82,7 @@ const loginUser = asyncHandler(async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       path: "/",
     });
